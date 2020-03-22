7bf3939eb2fa4ffec695ac5c20e3db02ea7136c5
Michael Pintos <machester4@gmail.com>
2020-03-21 22:49:18 -0300

    test updated

--- main_test.go
+++ main_test.go
@@ -3,12 +3,14 @@ package main
 import (
 	"testing"
 
-	"github.com/machester4/jane/chain"
+	"github.com/machester4/jane/lib"
 )
 
 func BenchmarkChain(b *testing.B) {
 	s := "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	lib.Initialize("chivito", "es-50")
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_ = chain.New(s)
+		lib.Recommend(s, "es-50", "chivito")
 	}
 }
